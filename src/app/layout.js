8adf24cd4cc670480fd207c95f52ab2a122103c7
DANKAMN <danielnicholas1477@gmail.com>
2024-11-20 15:49:23 +0100

    add next-themes to apply dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
+import { ThemeProvider } from "next-themes";
+import ThemeComponent from "./components/ThemeComponent";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,12 +22,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
+        <ThemeProvider>
+          <ThemeComponent>
+            <Header />
+            {children}
+          </ThemeComponent>
+        </ThemeProvider>
       </body>
     </html>
   );
